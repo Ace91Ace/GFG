d948f02bb60965d70b200c2dcaa07cecaa1d41e7
G Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2025-07-12 10:29:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Nth Catalan Number/nth-catalan-number.py
+++ Difficulty: Medium/Nth Catalan Number/nth-catalan-number.py
@@ -1,11 +1,15 @@
-
+from functools import lru_cache
 class Solution:
     def findCatalan(self, n):
         # code here
-        dp = [0]*(n+1)
-        dp[0] = dp[1] = 1
+        @lru_cache
+        def dfs(n):
+            if n <= 1:
+                return 1
+            res = 0
+            
+            for i in range(n):
+                res += dfs(i)*dfs(n-i-1)
+            return res
+        return dfs(n)
         
-        for i in range(2, n+1):
-            for j in range(i):
-                dp[i] += dp[j]*dp[i-j-1]
-        return dp[n]
