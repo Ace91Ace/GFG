91c6cd98854099327d133b676df5cbce85fb615a
G Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2025-07-29 15:13:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/ASCII Range Sum/ascii-range-sum.py
+++ Difficulty: Medium/ASCII Range Sum/ascii-range-sum.py
@@ -2,21 +2,16 @@ class Solution:
     def asciirange(self, s):
         n = len(s)
         occ = [[-1, -1] for _ in range(26)]
-        pref = [0] * n
         p = 0
         for i, x in enumerate(s):
             p += ord(x)
-            pref[i] = p
             idx = ord(x) - ord('a')
             if occ[idx][0] == -1:
-                occ[idx][0] = i
+                occ[idx][0] = p
             else:
-                occ[idx][1] = i
+                occ[idx][1] = p - ord(x)
         res = []
-        for i in range(26):
-            start, end = occ[i]
-            if start != -1 and end != -1 and end > start + 1:
-                y = pref[end-1] - pref[start]
-                if y:
-                    res.append(y)
+        for i in occ:
+            if i[1] != -1 and i[1] != i[0]:
+                res.append(i[1]-i[0])
         return res
