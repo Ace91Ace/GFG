20eb19b59f29fe784d0f8309312c6576843a0872
G Naga Vamshi <123461442+Ace91Ace@users.noreply.github.com>
2025-08-01 00:32:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Balancing Consonants and Vowels Ratio/balancing-consonants-and-vowels-ratio.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countBalanced(self, arr):
+        vow = set("aeiou")
+        res = []
+
+        for i in arr:
+            res.append(sum([1 if j in vow else -1 for j in i]))
+        
+        count = 0
+        pref = 0
+        dic = {0: 1}
+
+        for val in res:
+            pref += val
+            count += dic.get(pref, 0)
+            dic[pref] = dic.get(pref, 0) + 1
+
+        return count
